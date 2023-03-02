9a2db5ea7cb27ced9b949960b5f55bdd74a31083
foxstudent101212 <maxle130691@gmail.com>
2023-03-03 02:14:17 +0700

    update Angular v14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
+import {UntypedFormGroup, UntypedFormControl} from '@angular/forms';
 
 export interface Task {
   text: string
@@ -57,8 +57,8 @@ export class AppComponent implements OnInit {
     return `${activeSubtasks} subtasks`
   }
 
-  form = new FormGroup({
-    sort: new FormControl('All')
+  form = new UntypedFormGroup({
+    sort: new UntypedFormControl('All')
   })
 
   clearCompleted() {
