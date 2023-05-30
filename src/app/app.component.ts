eb7fc539349dd98bc943dc3de9343035f3b51898
foxstudent101212 <maxle130691@gmail.com>
2023-05-30 13:36:29 +0700

    feat: bootstrap icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-// import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +7,4 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'my-to-do';
-
-  // constructor(private modalService: NgbModal) {
-  // }
-  //
-  // public open(modal: any): void {
-  //   this.modalService.open(modal);
-  // }
 }
